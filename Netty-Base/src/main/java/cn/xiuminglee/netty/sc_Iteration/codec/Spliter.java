d58502b515a5970d04e7141b6944763c60fe0317
XIuming Lee <lxmcoder@gmail.com>
2019-09-09 09:49:36 +0800

    [update]- 添加注释

--- Netty-Base/src/main/java/cn/xiuminglee/netty/sc_Iteration/codec/Spliter.java
+++ Netty-Base/src/main/java/cn/xiuminglee/netty/sc_Iteration/codec/Spliter.java
@@ -9,7 +9,7 @@ import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
  * @Author: Xiuming Lee
  * @Date: 2019/8/12 19:21
  * @Version 1.0
- * @Describe:
+ * @Describe: 用于拒绝非本协议连接
  */
 public class Spliter extends LengthFieldBasedFrameDecoder {
     private static final int LENGTH_FIELD_OFFSET = 7;
@@ -22,8 +22,9 @@ public class Spliter extends LengthFieldBasedFrameDecoder {
 
     @Override
     protected Object decode(ChannelHandlerContext ctx, ByteBuf in) throws Exception {
+        // 屏蔽非本协议的客户端
         if (in.getInt(in.readerIndex()) != PacketCodeC.MAGIC_NUMBER) {
-            ctx.channel().close();
+            ctx.channel().close(); // 不符合我们定义的协议格式，关闭连接。
             return null;
         }
         return super.decode(ctx, in);
